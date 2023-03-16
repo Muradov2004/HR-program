20b8cd03f5108e8d296db2b800d0334da8602fef
Əli Muradov <ami65143@gmail.com>
2023-03-16 16:31:59 +0400

    Add project files.

---
+++ HR program/Human.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HR_program
+{
+    internal class Human
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public override string ToString() => $@"Name: {Name} Surname: {Surname} BirthDate: {BirthDate.Date}";
+    }
+}
---
+++ HR program/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace HR_program
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            if (txtName.Text != string.Empty && txtSurname.Text != string.Empty)
+            {
+                lstw.Items.Add(new { Name = txtName.Text, Surname = txtSurname.Text, BirthDate = txtDate.Text });
+                txtName.Clear();
+                txtSurname.Clear();
+                txtDate.Text = string.Empty;
+            }
+            else
+                MessageBox.Show("You can not enter empty name or surname", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            lstw.Items.Remove(lstw.SelectedItem);
+        }
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            txtName.Clear();
+            txtSurname.Clear();
+            txtDate.Text = string.Empty;
+            lstw.Items.Clear();
+        }
+    }
+}
